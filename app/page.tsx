119ef2b07716d83144a5a410dacc818e58a1d908
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-10 14:52:03 +0545

    css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Board from "@/components/Board";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-700 min-h-screen" >
+
       <Board/>
     </div>
   )
--- components/Board.tsx
+++ components/Board.tsx
@@ -15,9 +15,10 @@ function createChessboardArray() {
 function Board() {
     const board = createChessboardArray()
     return (
-        <div className="flex flex-col justify-center items-center" >
+        <div className="flex flex-col justify-center items-center">
+            <p className="text-7xl font-semibold text-gray-600" >CHESSBOARD</p>
             {board.map((rows, cols) => (
-                <div className={`flex bg-[#f4f4f4] ${cols % 2 ? 'striped-odd' : 'striped'}`} key={cols}>
+                <div className={`flex bg-[#f4f4f4] shadow-2xl shadow-cyan-400 ${cols % 2 ? 'striped-odd' : 'striped'}`} key={cols}>
                     {rows.map((col, index) => (
                         <div className={`w-[100px] h-[100px]`} key={index}>
                             <button className="w-full h-full text-gray-600">
